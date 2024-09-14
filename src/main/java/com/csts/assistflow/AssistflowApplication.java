a9da9168f3df72ad2e35e3210ec56bcdaabbbf8c
rachelshirin007 <161051063+vkumar7271@users.noreply.github.com>
2024-09-14 17:30:23 -0400

    first commit

---
+++ src/main/java/com/csts/assistflow/AssistflowApplication.java
@@ -0,0 +1,13 @@
+package com.csts.assistflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssistflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssistflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/csts/assistflow/AssistflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csts.assistflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
